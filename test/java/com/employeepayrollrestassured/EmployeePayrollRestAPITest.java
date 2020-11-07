4f577071f12ff39a3290def5f34162315640ca2c
Kanishk-kumar399 <kanishkk301198@gmail.com>
2020-11-07 17:03:41 +0530

    [Kanishk] ADDED new employee to the employeePayroll JSON server

--- test/java/com/employeepayrollrestassured/EmployeePayrollRestAPITest.java
+++ test/java/com/employeepayrollrestassured/EmployeePayrollRestAPITest.java
@@ -12,6 +12,7 @@ import com.google.gson.Gson;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class EmployeePayrollRestAPITest 
 {
@@ -37,4 +38,26 @@ public class EmployeePayrollRestAPITest
         long entries=employeePayrollREST_IOService.countREST_IOEntries();
         Assert.assertEquals(2,entries);
     }
+    @Test
+    public void givenNewEmployeeWhenAddedShouldMatch201ResponseAndcount()
+    {
+    	EmployeePayrollService employeePayrollService;
+    	EmployeePayrollData[] arrayOfEmployees=getEmployeeList();
+    	employeePayrollService=new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
+    	EmployeePayrollData employeePayrollData=new EmployeePayrollData(4,"bhinav",2223223.0);
+    	Response response=addEmployeeToJsonServer(employeePayrollData);
+    	int HTTPstatusCode=response.getStatusCode();
+    	Assert.assertEquals(201,HTTPstatusCode);
+    	employeePayrollData=new Gson().fromJson(response.asString(),EmployeePayrollData.class);
+    	employeePayrollService.addEmployeeToPayrollUsingRestAPI(employeePayrollData);
+    	long entries=employeePayrollService.countREST_IOEntries();
+    	Assert.assertEquals(4,entries);
+    }
+    public Response addEmployeeToJsonServer(EmployeePayrollData employeePayrollData) {
+		String employeeJson=new Gson().toJson(employeePayrollData);
+		RequestSpecification request=RestAssured.given();
+		request.header("Content-Type","application/json");
+		request.body(employeeJson);
+		return request.post("/employees");
+	}
 }
